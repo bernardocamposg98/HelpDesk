4d75d6ec5c5cba0afaab04e411dfbbf95a4d9caf
Alejandro <android24021998@gmail.com>
2019-07-08 19:15:26 -0500

    prueba

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
 <body>
+    <h1>title</h1>
     <div class="container">
         <div class="row justify-content-center">
             <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-10 col-lg-10 col-xl-6 col-sm-12">
